516de324f4860bcbf9315eac8aca3c1dc923fa09
Thiago Marques <thiago.siqueira@alticeusa.com>
2025-06-01 21:36:00 +0100

    created router for healthz

--- main.go
+++ main.go
@@ -7,6 +7,16 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	// Serve files under /app/
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
